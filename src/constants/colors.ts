d96832a493721f27658a19727a91b6ab3dc3f604
Nolan Moak <nolanmoak@live.com>
2024-12-12 15:30:32 -0500

    Added prettier, more constants, and pnpm enforced in package.json

---
+++ src/constants/colors.ts
@@ -0,0 +1,5 @@
+export default {
+  primary: '#0a4dda',
+  grey: '#5e5d5e',
+  dark: '#1a1a1a',
+};
--- src/constants/meditiation-images.ts
+++ src/constants/meditiation-images.ts
@@ -1,15 +1,8 @@
-import treeImage from "#/assets/images/meditation-images/trees.webp";
-import meditatingUnderTree from "#/assets/images/meditation-images/meditate-under-tree.webp";
-import riverImage from "#/assets/images/meditation-images/river.webp";
-import beachImage from "#/assets/images/meditation-images/beach.webp";
-import yosemiteStars from "#/assets/images/meditation-images/yosemite-stars.webp";
-import waterfall from "#/assets/images/meditation-images/waterfall.webp";
+import beachImage from '#/assets/images/meditation-images/beach.webp';
+import meditatingUnderTree from '#/assets/images/meditation-images/meditate-under-tree.webp';
+import riverImage from '#/assets/images/meditation-images/river.webp';
+import treeImage from '#/assets/images/meditation-images/trees.webp';
+import waterfall from '#/assets/images/meditation-images/waterfall.webp';
+import yosemiteStars from '#/assets/images/meditation-images/yosemite-stars.webp';
 
-export default [
-    treeImage,
-    riverImage,
-    meditatingUnderTree,
-    beachImage,
-    yosemiteStars,
-    waterfall,
-];
+export default [treeImage, riverImage, meditatingUnderTree, beachImage, yosemiteStars, waterfall];
